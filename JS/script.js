878f2aba93cae133c7659bdf0d0af55d6799a2d5
email <avto.m@ukr.net>
2020-12-06 18:37:42 +0200

    seve

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,5 @@
+
+
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 const personalMovieDB ={
     count:  numberOfFilms,
@@ -7,13 +9,28 @@ const personalMovieDB ={
     privat: false
 };
 
+for (let i=0; i<2; i++) {
+    a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = prompt('На сколько оцените его?', '');
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-b = prompt('На сколько оцените его?', ''),
-c = prompt('Один из последних просмотренных фильмов?', ''),
-d = prompt('На сколько оцените его?', '');
+    if (a !=null && b !=null && a !='' && b !='' && a.length <50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+    
+}
 
-personalMovieDB.movies [a] = b;
-personalMovieDB.movies [c] = d;
+if (personalMovieDB.count <10) {
+    console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <30) {
+    console.log( 'Вы красавчик');
+} else if (personalMovieDB.count >= 30) {
+    console.log ('Вы киноман');
+} else {
+    console.log('Произошла ошибка');
+}
 
 console.log(personalMovieDB);
